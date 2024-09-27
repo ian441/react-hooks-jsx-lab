2164d5be33bed0641ee78fd846f4166c4229ce8f
ian ayiro <ian.ayiro@student.moringa.com>
2024-09-27 11:50:56 +0300

    modified:   src/components/About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
 
+
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2> About me</h2>
+    <p>i love music</p>
+    <img src={image} alt="i made this"/>
+    </div>
+);
 }
 
 export default About;
